1390c722a30955d018848c444f4acecac4def6f5
Nirmalya Mandal <149013522+DevNiru2704@users.noreply.github.com>
2024-07-13 14:17:13 +0000

    Added new file 02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+alert(3+3); /*we are using node js not browser so error will happen. 
+            but there is another method in node js that we will
+            learn later on*/
+
+console.log(3+
+    3); //code readability should be the priority. don't use these kind of style. just because it can be done doesn't mean that it should be done.
+
+
+let name="Nirmalya"
+let age= 19
+let isLoggedIn=false;
+
+
+/*Types of data types:
+number=36
+bigint(for large numbers)
+string="Hello"
+boolean=true/false
+null = standalone value
+undefined
+symbol = for uniqueness
+
+
+object*/
+
+console.log(typeof age);
