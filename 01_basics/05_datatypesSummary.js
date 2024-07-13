1d868b7c43876e13bfbdb2d6488d0eacba923b53
Nirmalya Mandal <149013522+DevNiru2704@users.noreply.github.com>
2024-07-13 19:45:19 +0000

    Learn more about how memories work in javascript

--- 01_basics/05_datatypesSummary.js
+++ 01_basics/05_datatypesSummary.js
@@ -41,10 +41,53 @@ let bioData={
 
 //Funtion syntax. In JS we can also store function in variables.
 const functionVariable=function myFirstFunction(){
-    console.log("Hello World");
+    //console.log("Hello World");
 }
 
 //console.log(functionVariable); //The result will be exactly "[Function: myFirstFunction]" without the quotes.
 
-console.table([typeof heroes,typeof bioData,typeof functionVariable]); // for objects and array their types are object and
+//console.table([typeof heroes,typeof bioData,typeof functionVariable]); // for objects and array their types are object and
                                                                        // for function their type is object function.
+
+
+
+/* ********************************* How does memory work in Javascript? *********************************
+
+Types of memories:
+1.Stack(For primitive data types)
+2.Heap(For non-primitive data types.)
+
+Here's the link to my figma diagram for stack and heap in javascript. 
+https://www.figma.com/design/g2qeQUDsMwiccjkDV8CiPw/Types-of-memories-in-javascript?node-id=0-1&t=vFu9rv2hDApE7BAq-0
+*/
+
+let myName="Nirmalya Mandal"; //As soon as a primitive data variable is declared it, the memory assigned to it is a stack
+                              //memory just like the diagram from the link.
+
+let anotherName=myName;
+//console.log(anotherName);
+
+anotherName="John Doe"
+//console.log(anotherName);
+//console.log(myName); //We see that myName is not modified even though we gave the reference of myName in line 67.
+                     //This is because in stack we are provided with a copy of myName instead of the original one
+
+//For Reference Types:
+let userOne={
+    name:"Nirmalya Mandal",
+    age:"19"
+}
+
+let userTwo={
+    name:"John Doe",
+    age:"20"
+}
+
+userTwo=userOne
+userTwo.name="Ram"
+//console.table([userOne,userTwo]); //We can see that when changing the name inside the  userTwo object, the name inside
+                                  //the userOne object is also changed. This is because unlike stack, objects share
+                                  //the heap memory where two or more non-priitive data types can share one reference
+                                  //unlike the primitive data types.
+
+ 
