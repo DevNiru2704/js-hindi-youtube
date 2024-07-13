2694282c1df736e65b028e13cb8b761a5343ef92
Nirmalya Mandal <149013522+DevNiru2704@users.noreply.github.com>
2024-07-13 19:47:40 +0000

    small change

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,8 +4,8 @@ var accountPassword="12345";
 accountCity="Jaipur"; //not a recommended way to initialize a variable but still possible
 let accountState;
 //accountId=2 not allowed because of const.
-console.log(accountId);
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+//console.log(accountId);
+//console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
 
 
 /* Prefer not to use var because of issue in block scope and functional scope. */
