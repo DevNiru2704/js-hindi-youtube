b2efde72cbaa786e9e2233c9314426e4c771cb2b
Nirmalya Mandal <149013522+DevNiru2704@users.noreply.github.com>
2024-07-13 15:15:59 +0000

    Added some notes to 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -9,3 +9,10 @@ console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
 
 
 /* Prefer not to use var because of issue in block scope and functional scope. */
+
+//var is function() scoped and let is braces{} scoped. That mean we can use var anywhere inside a function and we can
+//use let only inside a particular curly braces. That means declaring a variable using let has the same function as the
+//variables in other programming languages like C, C++, etc. Also let is more secure compared to var because if we open
+//the console inside a browser and type inside "window" without the quotes. We can see our variable along with its data
+//inside the window. So using var someone with malicious intentions can easily see our data inside our website by this
+//method.
