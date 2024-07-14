c15c5a87c2cc38a962301b75bd34beea70420cd4
Nirmalya Mandal <149013522+DevNiru2704@users.noreply.github.com>
2024-07-14 20:03:00 +0000

    Learnt about arrays

---
+++ 02_basics/01_array.js
@@ -0,0 +1,29 @@
+const myArr=[0,1,2,3,4,5];
+const heroes=["shaktiman","superman","batman"];
+const myArr2=new Array(1,2,3,4,5);
+// console.log(myArr[0]);
+
+// myArr.push(6);
+// myArr.push(7);
+// console.log(myArr);
+// myArr.pop();
+// console.log(myArr);
+
+// myArr.unshift(-1);
+// console.log(myArr);
+// myArr.shift();
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+const newArr=myArr.join();
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+console.log("A ",myArr);
+console.log(myArr.slice(0,3)); //It does not directly modify the array.
+console.log("B ",myArr);
+console.log(myArr.splice(0,3)); //It directly modifies the array.
+console.log("C ",myArr);
