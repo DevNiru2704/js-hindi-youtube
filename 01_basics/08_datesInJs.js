63c5fd85a899af611ef89f1d079c6e3d7e0df23a
Nirmalya Mandal <149013522+DevNiru2704@users.noreply.github.com>
2024-07-14 19:05:27 +0000

    learned about dates

---
+++ 01_basics/08_datesInJs.js
@@ -0,0 +1,47 @@
+let myDate=new Date();
+// console.log(myDate.toString()); //Result= Sun Jul 14 2024 18:34:06 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toDateString()); //Result= Sun Jul 14 2024
+// console.log(myDate.toISOString()); //Result= 2024-07-14T18:34:06.163Z
+// console.log(myDate.toJSON()); //Result= 2024-07-14T18:34:06.163Z
+// console.log(myDate.toLocaleDateString()); //Result= 7/14/2024
+// console.log(myDate.toLocaleString()); //Result= 7/14/2024, 6:34:06 PM
+// console.log(myDate.toLocaleTimeString()); //Result= 6:34:06 PM
+// console.log(myDate.toTimeString()); //Result= 18:34:06 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toUTCString()); //Result= Sun, 14 Jul 2024 18:34:06 GMT
+
+
+let createdDate=new Date(2023,0,23);
+// let createdDate=new Date(2023,0,23,5,3,50);
+// let createdDate=new Date("2024-07-15"); //yyyy-mm-dd
+// let createdDate=new Date("07-15-2024"); //mm-dd-yyyy
+// console.log(createdDate.toString());
+
+// let myTimeStamp=Date.now();
+// console.log(myTimeStamp);
+// console.log(createdDate.getTime())
+// console.log(Math.trunc(Date.now()/1000));
+
+
+let newDate=new Date();
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());
+// console.log(newDate.getFullYear());
+// console.log(newDate.getHours());
+// console.log(newDate.getMilliseconds());
+// console.log(newDate.getMinutes());
+// console.log(newDate.getMonth());
+// console.log(newDate.getSeconds());
+// console.log(newDate.getTime());
+// console.log(newDate.getTimezoneOffset());
+// console.log(newDate.getUTCDate());
+// console.log(newDate.getUTCDay());
+// console.log(newDate.getUTCFullYear());
+// console.log(newDate.getUTCHours());
+// console.log(newDate.getUTCMilliseconds());
+// console.log(newDate.getUTCMinutes());
+// console.log(newDate.getUTCMonth());
+// console.log(newDate.getUTCSeconds());
+
+// newDate.toLocaleString('default',{
+//     weekday: "long"
+// })
