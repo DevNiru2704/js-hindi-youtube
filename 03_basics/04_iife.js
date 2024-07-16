38f682a8a2d7a1c452d6bf24bcd7b3ccf8a62ba2
Nirmalya Mandal <149013522+DevNiru2704@users.noreply.github.com>
2024-07-16 15:23:19 +0000

    minor changes

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -2,9 +2,13 @@
 
 (function chai(){ //named IIFE
     console.log(`DB CONNECTED ONE`)
-})(); //()() The first parenthesis is where is our function is located and the second parenthesis is our execution. IIFE helps in avoiding global scope pollution by encapsulating code within a local scope. This is especially useful in JavaScript where global variables can easily lead to conflicts. Also for IIFE, we must use the semicolon to avoid runtime errors.
+})(); //()() The first parenthesis is where is our function is located and the second parenthesis is our execution. IIFE helps in avoiding global scope pollution by encapsulating code within a local scope. This is especially useful in JavaScript where global variables can easily lead to conflicts. Also for multiple IIFEs, we must use the semicolon to avoid runtime errors.
 
-(()=>(console.log(`DB CONNECTED TWO`)))();
+(()=>(console.log(`DB CONNECTED TWO`)))(); //unnamed IIFE using arrow function
+
+(function(){ //unnamed IIFE using function keyword
+    console.log(`DB CONNECTED THREE`)
+})();
 
 ((name)=>(console.log(`Welcome, my name is ${name}`)))("Nirmalya");
 
