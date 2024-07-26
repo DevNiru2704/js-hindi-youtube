1a1af6bc55bc428091e27e2493119b33c2a53cdb
Nirmalya Mandal <149013522+DevNiru2704@users.noreply.github.com>
2024-07-26 16:21:08 +0000

    Learnt more about getter,setter, getOwnPropertyDescriptor function, bind,  lexical scoping and closure

--- test.js
+++ test.js
@@ -1,13 +1,12 @@
 let str="            Apple              "
-// console.log(str.length)
-// console.log(str.trim().length)
-// String.prototype.trueLength=function(){
-//     return this.trim().length
-// }
-
-// console.log(str.trueLength())
-// console.log("               Nirmalya            ".length)
-// console.log("               Nirmalya            ".trueLength())
+console.log(str.length)
+console.log(str.trim().length)
+String.prototype.trueLength=function(){
+    return this.trim().length
+}
+console.log(str.trueLength())
+console.log("               Nirmalya            ".length)
+console.log("               Nirmalya            ".trueLength())
 
 Object.prototype.helloMessage=function(){
     console.log("Hello this is a function made by me inside the object constructor prototype.")
